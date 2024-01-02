ff98bc619a3d2e2b130adcf353f5c45fa4843c7e
amira-hamed <127278706+amira-hamed@users.noreply.github.com>
2024-01-02 15:54:22 +0400

    Update finding_regions.sh

--- finding_regions.sh
+++ finding_regions.sh
@@ -24,10 +24,10 @@ cat cov.merged15.bed | cut -f 1,2,3 > goat2sheep.bed
 bedtools intersect -a goat2sheep.bed -b real_sheep.bed -v | bedtools merge > sheep_uniq.bed
 sort -k 1,1 sheep_uniq.bed > sheep_uniq.sorted.bed
 bedtools genomecov -bga -i sheep_uniq.sorted.bed -g sheep.genome | awk '$4 >0' > sheep_uniq.merged.bed
-cat sheep_uniq.merged.bed | awk '{print $1, $2, $3, $3 - $2}' | sed -e 's/ /\t/g' | sort -k4 -n -r | grep -v 'NW' | awk '{print $1":"$2"-"$3}' > sheep_specific_regions.bed
+cat sheep_uniq.merged.bed | awk '{print $1, $2, $3, $3 - $2}' | sed -e 's/ /\t/g' | sort -k4 -n -r | grep -v 'NW' | awk '{print $1":"$2"-"$3}' > sheep_specific_regions_full.bed
 
 ## Calculate coverage for 1000 regions
-for region in `cat sheep_specific_regions.bed | head -1000` 
+for region in `cat sheep_specific_regions_full.bed | head -1000` 
 do 
 	total=0
 	echo -n $region >> coverage_matrix_sheep.tsv
@@ -81,10 +81,10 @@ cat cov.merged15.bed | cut -f 1,2,3 > sheep2goat.bed
 bedtools intersect -a sheep2goat.bed -b real_goat.bed -v | bedtools merge > goat_uniq.bed
 sort -k 1,1 goat_uniq.bed > goat_uniq.sorted.bed
 bedtools genomecov -bga -i goat_uniq.sorted.bed -g goat.genome | awk '$4 >0' > goat_uniq.merged.bed
-cat goat_uniq.merged.bed | awk '{print $1, $2, $3, $3 - $2}' | sed -e 's/ /\t/g' | sort -k4 -n -r | grep -v 'NW' | awk '{print $1":"$2"-"$3}' > goat_specific_regions.bed
+cat goat_uniq.merged.bed | awk '{print $1, $2, $3, $3 - $2}' | sed -e 's/ /\t/g' | sort -k4 -n -r | grep -v 'NW' | awk '{print $1":"$2"-"$3}' > goat_specific_regions_full.bed
 
 ## Calculate coverage for 1000 regions
-for region in `cat goat_specific_regions.bed | head -1000` 
+for region in `cat goat_specific_regions_full.bed | head -1000` 
 do 
 	total=0
 	echo -n $region >> coverage_matrix_goat.tsv
