4788f00906ebdc886696bafb49c07908e3755f65
amira-hamed <127278706+amira-hamed@users.noreply.github.com>
2024-01-02 15:46:49 +0400

    Update finding_regions.sh

--- finding_regions.sh
+++ finding_regions.sh
@@ -26,6 +26,35 @@ sort -k 1,1 sheep_uniq.bed > sheep_uniq.sorted.bed
 bedtools genomecov -bga -i sheep_uniq.sorted.bed -g sheep.genome | awk '$4 >0' > sheep_uniq.merged.bed
 cat sheep_uniq.merged.bed | awk '{print $1, $2, $3, $3 - $2}' | sed -e 's/ /\t/g' | sort -k4 -n -r | grep -v 'NW' | awk '{print $1":"$2"-"$3}' > sheep_specific_regions.bed
 
+## Calculate coverage for 1000 regions
+for region in `cat sheep_specific_regions.bed | head -1000` 
+do 
+	total=0
+	echo -n $region >> coverage_matrix_sheep.tsv
+	echo -n -e '\t' >> coverage_matrix_sheep.tsv
+	
+	tmp=$(echo $region | cut -d ':' -f 2 )
+
+	length=$(echo $((tmp*-1)))
+	
+	for bedfile in `ls samples_bed_files`
+	do 
+		sample=$(echo $bedfile | sed -e 's/.bed//g')
+
+		average=$(samtools depth -r $region /path/to/sample/alignment/folder/${sample}/${sample}_deduped.bam | awk -v length2="$length" '{sum += $3} END {print sum/length2}' )
+
+		total=$(awk "BEGIN {print $total + $average}")
+	done
+
+	result=$(awk "BEGIN {print $total / 15}")
+
+	echo "$result" >> cov_matrix_sheep.tsv
+done
+
+## further step to decide the top 10 regions to view in IGV
+cat cov_matrix_sheep.tsv | sort -k2 -n | head -10 | cut -f 1
+## After further manual checking, the top 10 regions are saved to "sheep_specific_regions_selected.bed" 
+
 ################################### Goat ###################################
 ## get regions that has a coverage of < 10 for real_goat samples
 bedtools genomecov -bga -ibam $bamfile | awk '$4<10' > ${sample}.bed
@@ -53,3 +82,32 @@ bedtools intersect -a sheep2goat.bed -b real_goat.bed -v | bedtools merge > goat
 sort -k 1,1 goat_uniq.bed > goat_uniq.sorted.bed
 bedtools genomecov -bga -i goat_uniq.sorted.bed -g goat.genome | awk '$4 >0' > goat_uniq.merged.bed
 cat goat_uniq.merged.bed | awk '{print $1, $2, $3, $3 - $2}' | sed -e 's/ /\t/g' | sort -k4 -n -r | grep -v 'NW' | awk '{print $1":"$2"-"$3}' > goat_specific_regions.bed
+
+## Calculate coverage for 1000 regions
+for region in `cat goat_specific_regions.bed | head -1000` 
+do 
+	total=0
+	echo -n $region >> coverage_matrix_goat.tsv
+	echo -n -e '\t' >> coverage_matrix_goat.tsv
+	
+	tmp=$(echo $region | cut -d ':' -f 2 )
+
+	length=$(echo $((tmp*-1)))
+	
+	for bedfile in `ls samples_bed_files`
+	do 
+		sample=$(echo $bedfile | sed -e 's/.bed//g')
+
+		average=$(samtools depth -r $region /path/to/sample/alignment/folder/${sample}/${sample}_deduped.bam | awk -v length2="$length" '{sum += $3} END {print sum/length2}' )
+
+		total=$(awk "BEGIN {print $total + $average}")
+	done
+
+	result=$(awk "BEGIN {print $total / 15}")
+
+	echo "$result" >> cov_matrix_goat.tsv
+done
+
+## further step to decide the top 10 regions to view in IGV
+cat cov_matrix_goat.tsv | sort -k2 -n | head -10 | cut -f 1
+## After further manual checking, the top 10 regions are saved to "goat_specific_regions_selected.bed" 
