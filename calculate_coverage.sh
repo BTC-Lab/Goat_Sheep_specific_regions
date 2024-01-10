e609d26c635288d62e771cf3b15a094c05bda3b0
amira-hamed <127278706+amira-hamed@users.noreply.github.com>
2024-01-10 09:22:03 +0400

    Update calculate_coverage.sh

--- calculate_coverage.sh
+++ calculate_coverage.sh
@@ -1,6 +1,12 @@
 regions_bed="$1"
 bamFile="$2"
 
+echo "## command= sh calculate_coverage.sh ${regions_bed} ${bamFile} "
+echo -e "##chrom\tstart\tend\tcoverage" 
+
+lines=$(cat $regions_bed | wc -l )
+covavg=0
+
 for region in `awk '{print $1":"$2"-"$3}' $regions_bed`
 do
 	tmp=$(echo $region | cut -d ':' -f 2 )
@@ -10,6 +16,23 @@ do
 	echo -n $(echo $region | sed 's/-/:/g' | sed 's/:/\t/g') 
 	echo -n -e "\t" 
 	echo $average 
+	
+	covavg=$(awk "BEGIN {print $covavg + $average}")
 done
+
+
+result=$(awk "BEGIN {print $covavg / $lines}")
+
+echo ""
+echo "## average coverage for the given regions is: ${result}"
+
+i=$(printf '%.0f' "$result")
+
+if [ $i -le 1 ]; then
+echo "## warning: average coverage is < 1, the sample does not seem to match with current reference genome, alignment to another reference genome is recommended" 
+else
+echo "## if average coverage is < 1, the sample does not seem to match with current reference genome, alignment to another reference genome is recommended" 
+fi
+
 
 
